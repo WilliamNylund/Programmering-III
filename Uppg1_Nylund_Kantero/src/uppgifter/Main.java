5eeda0d4714238671b1aac1624ab4738daf19216
William Nylund <william.nylund@gmail.com>
2019-03-17 18:53:43 +0200

    Removed unnecessary comments

--- Uppg1_Nylund_Kantero/src/uppgifter/Main.java
+++ Uppg1_Nylund_Kantero/src/uppgifter/Main.java
@@ -2,13 +2,11 @@ package uppgifter;
 
 
 public class Main {
-	//testedit2
+
 	public static void main(String[] args){
-		//testedit
+
 		Window window = new Window();
-		//testedit from eclipse
-		//testedit from github
-		//anotha one from eclipse
+
 	}
 
 }
